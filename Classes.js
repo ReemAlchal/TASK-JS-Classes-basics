d8b7137c30480dc1610f5a1d504b4c16a0036ac2
ReemAlchal <reemalchal@hotmail.com>
2023-03-12 17:59:40 +0300

    Classes and Inheritance

--- Classes.js
+++ Classes.js
@@ -4,7 +4,6 @@
  * - lastName: String
  * - gender: String
  * - birthYear: Number
- *
  * 2. Add the constructor that initializes all properties
  *
  * 3. Add the following methods
@@ -20,9 +19,43 @@
  * print out the sum of their ages using calculateAge() method
  */
 class Person {
-  // continue the code here
-}
+  constructor(firstName,lastName,gender,birthday){
+    this.firstName=firstName
+    this.lastName=lastName
+    this.gender=gender
+    this.birthday=birthday
+  }
 
+  printName(){
+    console.log(`${this.firstName} ${this.lastName}`)
+  }
+  calculateAge(currantYear){
+    let age= currantYear-this.birthday
+    return age
+  }
+}
+const person1=new Person('Reem','Alchal','Female','1999')
+const person2=new Person('Doha','Samir','Female','2001')
+const person3=new Person('Mohamad','Alsaied','Male','2006')
+const person4=new Person('Mohamad','Alsaied','Male','2006')
+const person5=new Person('Mohamad','Alsaied','Male','2006')
+const person6=new Person('Mohamad','Alsaied','Male','2006')
+const person7=new Person('Mohamad','Alsaied','Male','2006')
+
+person1.printName()
+person2.printName()
+person3.printName()
+
+const sum = (person1.calculateAge(2023))+(person2.calculateAge(2023))+(person3.calculateAge(2023))
+console.log(sum)
+
+function getSum(persons)
+{
+  let ageSum = 0;
+  persons.forEach(person=> ageSum+= person.calculateAge(2023))
+  return ageSum
+}
+console.log(getSum([person1, person2, person3, person5, person4, person6, person7]))
 /** (Question 2): (15000 Points)
  * 1. Write a class `Movie`, give it the following properties
  * - title
@@ -45,9 +78,23 @@ class Person {
  */
 
 class Movie {
-  // continue the code here
+  rating=[]
+  constructor(title,duration,genre){
+    this.title=title
+    this.duration=duration
+    this.genre=genre
+  }
+  rate(rate){
+    if(rate>=0 && rate<=10){
+      this.rating.push(rate)
+    }
+  }
+averageRating(){
+  let rates = 0;
+  this.rating.forEach(rate=> rate+=rates)
+  return rates/this.rating.length
 }
-
+}
 /** (Question 3): (1000 Points)
  * 1. Create a class `Actor` that inherits `Person`, and adds the following properties
  * - movies: array of `Movie`
@@ -58,3 +105,16 @@ class Movie {
  */
 
 // write the class here
+
+class Actor extends Person{
+  constructor(movies){
+    this.movies=[]
+    super (firstName,lastName,gender,birthday)
+  }
+  addMovie(movieName){
+    this.movies.push(movieName)
+  }
+  }
+
+
+
